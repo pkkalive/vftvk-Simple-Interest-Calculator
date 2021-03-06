41143d9a16c215a97231116f264d1b02ab3ed5a9
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2021-03-06 15:03:05 +0530

    Assignment done by Purushotham

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function compute() {
-  let principal = document.getElementById("principal").value;
-  if (principal<=0){
-    document.getElementById("result").innerHTML = "Please enter a valid amount positive number for example 1000.";
+  let principal = document.getElementById("principal");
+  if (principal.value <= 0){
+    alert("Please enter a valid amount positive number for example 1000.");
+    principal.focus();
     return;
   }
   let rate = document.getElementById("rate").value;
   let years = document.getElementById("years").value;
-  let interest = (parseInt(principal) * parseFloat(rate) * parseInt(years))/100;
+  let interest = (parseInt(principal.value) * parseFloat(rate) * parseInt(years))/100;
   let currentDate = new Date();
   let year = currentDate.getFullYear();
   let maturityYear = parseInt(years) + parseInt(year);
-  let maturityValue = parseInt(interest) + parseInt(principal);
+  let maturityValue = parseInt(interest) + parseInt(principal.value);
   let output = "If you deposit <mark>" 
-        + principal + "</mark>,<br>at an interest rate of <mark>" 
+        + principal.value + "</mark>,<br>at an interest rate of <mark>" 
         + rate +"%</mark>.<br>You will receive an amount of <mark>" 
         + maturityValue + "</mark>,<br> in the year <mark>" 
         + maturityYear + "</mark><br>";
